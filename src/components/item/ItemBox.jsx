d776e8deead35f0038ee85ceb662856688ad31ce
Hang Vuong <vuonghalyn@gmail.com>
2022-05-08 21:03:21 +0300

    add and style itemBox component

---
+++ src/components/item/ItemBox.jsx
@@ -0,0 +1,19 @@
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import styles from "./itemBox.module.css";
+const ItemBox = ({ item, index }) => {
+  return (
+    <div className={styles.container}>
+      <div>
+        <p>{item.name}</p>
+        <p>{item.brewery_type}</p>
+        <p>{item.city}</p>
+      </div>
+      <Link key={index} className={styles.button} to={`/detail/${item.id}`}>
+        View Detail
+      </Link>
+    </div>
+  );
+};
+
+export default ItemBox;
