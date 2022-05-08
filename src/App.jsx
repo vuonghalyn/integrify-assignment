43efb83c8c4f1ea8cc56dbcb76515c1f385fff48
Hang Vuong <vuonghalyn@gmail.com>
2022-05-08 21:02:28 +0300

    init project with CRA

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import DetailPage from "./pages/DetailPage";
+import HomePage from "./pages/HomePage";
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/detail/:id" element={<DetailPage />} />
+
+      {/* Using path="*"" means "match anything", so this route
+          acts like a catch-all for URLs that we don't have explicit
+          routes for. */}
+      {/* <Route path="*" element={<NoMatch />} /> */}
+    </Routes>
+  );
+};
+
+export default App;
